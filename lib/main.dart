3d2e985bc061bfb8ad5428a600db698b188b127c
softproto <softproto@gmail.com>
2022-08-01 11:38:53 +0300

    implement draw by the path

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,178 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 // import 'dart:math' as math;
 
+void main() => runApp(
+      const MaterialApp(
+        home: AnimatedPathDemo(),
+      ),
+    );
+
+class AnimatedPathPainter extends CustomPainter {
+  final Animation<double> _animation;
+
+  AnimatedPathPainter(this._animation) : super(repaint: _animation);
+
+  Path _createAnyPath(Size size) {
+    return Path()
+      ..moveTo(size.height / 4, size.height / 4)
+      ..lineTo(size.height, size.width / 2)
+      ..lineTo(size.height / 2, size.width)
+      ..quadraticBezierTo(size.height / 2, 100, size.width, size.height)
+      ..lineTo(size.height / 2, size.width / 2)
+      ..quadraticBezierTo(200, 200, 100, 300);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final animationPercent = _animation.value;
+
+    // print("Painting + $animationPercent - $size");
+
+    final path = createAnimatedPath(_createAnyPath(size), animationPercent);
+
+    final Paint paint = Paint();
+    paint.color = Colors.amberAccent;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 10.0;
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class AnimatedPathDemo extends StatefulWidget {
+  const AnimatedPathDemo({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedPathDemoState createState() => _AnimatedPathDemoState();
+}
+
+class _AnimatedPathDemoState extends State<AnimatedPathDemo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  void _startAnimation() {
+    _controller.stop();
+    _controller.reset();
+    _controller.repeat(
+      period: const Duration(seconds: 5),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Animated Paint')),
+      body: SizedBox(
+        height: 300,
+        width: 300,
+        child: CustomPaint(
+          painter: AnimatedPathPainter(_controller),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _startAnimation,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+Path createAnimatedPath(
+  Path originalPath,
+  double animationPercent,
+) {
+  // ComputeMetrics can only be iterated once!
+  final totalLength = originalPath
+      .computeMetrics()
+      .fold(0.0, (double prev, PathMetric metric) => prev + metric.length);
+
+  final currentLength = totalLength * animationPercent;
+
+  return extractPathUntilLength(originalPath, currentLength);
+}
+
+Path extractPathUntilLength(
+  Path originalPath,
+  double length,
+) {
+  var currentLength = 0.0;
+
+  final path = Path();
+
+  var metricsIterator = originalPath.computeMetrics().iterator;
+
+  while (metricsIterator.moveNext()) {
+    var metric = metricsIterator.current;
+
+    var nextLength = currentLength + metric.length;
+
+    final isLastSegment = nextLength > length;
+    if (isLastSegment) {
+      final remainingLength = length - currentLength;
+      final pathSegment = metric.extractPath(0.0, remainingLength);
+
+      path.addPath(pathSegment, Offset.zero);
+      break;
+    } else {
+      // There might be a more efficient way of extracting an entire path
+      final pathSegment = metric.extractPath(0.0, metric.length);
+      path.addPath(pathSegment, Offset.zero);
+    }
+
+    currentLength = nextLength;
+  }
+
+  return path;
+}
+
+
+
+
 // void main() => runApp(MyApp());
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.indigo,
-      appBar: AppBar(
-        title: const Text(
-          "APP",
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          "My first app",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-      ),
-    ),
-  ));
-}
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       backgroundColor: Colors.indigo,
+//       appBar: AppBar(
+//         title: const Text(
+//           "APP",
+//           style: TextStyle(color: Colors.black),
+//         ),
+//         centerTitle: true,
+//       ),
+//       body: const Center(
+//         child: Text(
+//           "My first app",
+//           style: TextStyle(
+//             fontSize: 30,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.black,
+//           ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
 
 // class MyApp extends StatelessWidget {
 //   @override
